caf89e9523cbd39966aee2fbc1a28da078611406
Gustavo Teixeira <gustavoalmeidateixeira@gmail.com>
2021-07-22 22:23:32 -0300

    database configuration

---
+++ app/persistence/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+
+
+SQLALCHEMY_DATABASE_URL = ''
+
+#environment variable for database connection
+from .databasesettings import *
+
+if(SQLALCHEMY_DATABASE_URL.contains('sqlite://')):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine,)
+
+Base = declarative_base()
