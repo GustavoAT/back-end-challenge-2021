6bca01e1b3e2a837d6f8676343d18ed86d86c3b2
Gustavo Teixeira <gustavoalmeidateixeira@gmail.com>
2021-07-22 22:23:58 -0300

    initial user modelation

---
+++ app/persistence/models.py
@@ -0,0 +1,54 @@
+from sqlalchemy import Column, Integer, String, Enum, Float
+from sqlalchemy import DateTime, SmallInteger
+from .database import Base
+import enum
+
+
+
+
+class GendersEnum(enum.Enum):
+    male = enum.auto()
+    female = enum.auto()
+
+
+class StatusEnum(enum.Enum):
+    draft = enum.auto()
+    trash = enum.auto()
+    published = enum.auto()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    gender = Column(Enum(GendersEnum))
+    title = Column(String(4))
+    first_name = Column(String(20), nullable=False)
+    last_name = Column(String(20), nullable=False)
+    loc_street = Column(String(40))
+    loc_city = Column(String(30))
+    loc_state = Column(String(30))
+    loc_postcode = Column(String(20))
+    coordinates_latitude = Column(Float)
+    coordinates_longitude = Column(Float)
+    timezone_offset = Column(String(6))
+    timezone_description = Column(String(20))
+    email = Column(String(30), unique=True)
+    login_uuid = Column(String(40), unique=True)
+    login_username = Column(String(20), unique=True, nullable=False)
+    login_password = Column(String(70), nullable=False)
+    login_salt = Column(String(20), nullable=False)
+    date_of_birth = Column(DateTime, nullable=False)
+    age = Column(SmallInteger)
+    registered_date = Column(DateTime)
+    registered_age = Column(SmallInteger)
+    phone = Column(String(15))
+    cell = Column(String(15))
+    id_name = Column(String(10))
+    id_value = Column(String(20), unique=True)
+    picture_large = Column(String(60))
+    picture_medium = Column(String(60))
+    picture_thumbnail = Column(String(60))
+    nat = Column(String(2))
+    imported_t = Column(DateTime, nullable=False)
+    status = Column(Enum(StatusEnum), nullable=False)
---
+++ app/persistence/pdmodels.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+
+
+
+class UserBase(BaseModel):
+    gender: str
+    title: Optional[str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    loc_street: Optional[str] = None
+    loc_city: Optional[str] = None
+    loc_state: Optional[str] = None
+    loc_postcode: Optional[str] = None
+    coordinates_latitude: Optional[float] = None
+    coordinates_longitude: Optional[float] = None
+    timezone_offset: Optional[str] = None
+    timezone_description: Optional[str] = None
+    email: str
+    login_uuid: str
+    login_username: str
+    date_of_birth: datetime
+    age: Optional[int] = None
+    registered_date: Optional[datetime] = None
+    registered_age: Optional[int] = None
+    phone: Optional[str] = None
+    cell: Optional[str] = None
+    id_name: Optional[str] = None
+    id_value: Optional[str] = None
+    picture_large: Optional[str] = None
+    picture_medium: Optional[str] = None
+    picture_thumbnail: Optional[str] = None
+    nat: Optional[str] = None
+    imported_t:datetime
+    status:str
+
+
+class UserCreate(UserBase):
+    login_password:str
+    login_salt:str
+
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+        
