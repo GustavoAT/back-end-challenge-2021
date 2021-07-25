564db5343817c5ff026d2bfe9d72f7e2c7c22800
Gustavo Teixeira <gustavoalmeidateixeira@gmail.com>
2021-07-25 19:20:12 -0300

    update and delete functions

--- app/persistence/userDAO.py
+++ app/persistence/userDAO.py
@@ -3,6 +3,7 @@
     CRUD operations on database.
     """
 from sqlalchemy.orm import Session
+from sqlalchemy import update
 from . import models, pdmodels
 from .security.hash import create_salt, get_hashed_password
 
@@ -18,18 +19,25 @@ def get_users(db: Session, skip: int = 0, limit: int = 20):
 
 
 def create_user(db: Session, user: pdmodels.UserCreate):
-    salt = create_salt()
-    user.login_password = get_hashed_password(user.login_password, salt)
-    db_user = models.User(**user.dict(), login_salt=salt)
+    if(user.login_salt == None or user.login_salt == ''):
+        user.login_salt = create_salt()
+    user.login_password = get_hashed_password(user.login_password, user.login_salt)
+    db_user = models.User(**user.dict())
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
 
-def update_user():
-    pass
+def update_user(db: Session, user_id: int, user: pdmodels.UserBase):
+    update_statement = update(models.User).where(models.User.id == user_id).\
+        values(**user.dict()).execution_options(synchronize_session='fetch')
+    result = db.execute(update_statement)
+    return result
 
 
-def delete_user():
-    pass
+def delete_user(db: Session, user_id: int):
+    db_user = get_user(db, user_id)
+    db.delete(db_user)
+    db.commit()
+    return True
