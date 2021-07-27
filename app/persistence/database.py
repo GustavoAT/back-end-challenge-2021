4191e3a565de550149511ffaa30599b53688d26c
Gustavo Teixeira <gustavoalmeidateixeira@gmail.com>
2021-07-27 17:31:55 -0300

    database engine creation fix and comment

--- app/persistence/database.py
+++ app/persistence/database.py
@@ -10,7 +10,7 @@ SQLALCHEMY_DATABASE_URL = ''
 #environment variable for database connection
 from .databasesettings import *
 
-if('sqlite://' in SQLALCHEMY_DATABASE_URL):
+if 'sqlite://' in SQLALCHEMY_DATABASE_URL:
     engine = create_engine(
         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
     )
